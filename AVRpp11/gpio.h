b3b80ec701c688e4e9d9e1444bc86e54ef987416
Leph <leph.quentinrouxel@gmail.com>
2014-03-12 10:02:49 +0100

    New Gpio API with global const Object Pin

--- AVRpp11/gpio.h
+++ AVRpp11/gpio.h
@@ -8,95 +8,73 @@ namespace gpio {
  */
 enum GpioMode : uint8_t
 {
-    Input = 0,
-    Output = 1,
-    InputPullUp = 2,
+    Input,
+    Output,
+    InputPullUp,
 };
 
 /**
- * Gpio data structure for
- * runtime mapping
+ * Data structure
+ * representing Gpio Pin
  */
-struct GpioMappingEntry
+struct GpioEntry
 {
+    /**
+     * Direction, output and input
+     * register and bit number
+     */
     const bytePtr dirReg;
     const bytePtr outReg;
     const bytePtr inReg;
     const bits::BitNum num;
+
+    /**
+     * Set the Pin to given mode
+     */
+    inline void mode(GpioMode mode) const
+    {
+        if (mode == Input) {
+            bits::clear(*dirReg, num);
+        } else if (mode == Output) {
+            bits::add(*dirReg, num);
+        } else if (mode == InputPullUp) {
+            bits::clear(*dirReg, num);
+            bits::add(*outReg, num);
+        }
+    }
+    
+    /**
+     * Write given logical value to the
+     * Gpio Pin (should be Output mode)
+     */
+    inline void write(logic value) const
+    {
+        bits::set(*outReg, num, value);
+    }
+
+    /**
+     * Read and return the logical value
+     * of the Gpio Pin (should be in Input mode)
+     */
+    inline logic read() const
+    {
+        return bits::get(*inReg, num);
+    }
 };
 
 /**
- * Gpio pin declaration
- * for device specific layout
+ * Define const global object
+ * as Gpio Pin
  */
-enum GpioPin : uint8_t
-{
-    #define X(Pin, Port, Num) Pin,
-    GPIO_DEFINES
-    #undef X
-};
-
-/**
- * Gpio runtime global array
- * with device specific mapping
- */
-const GpioMappingEntry GpioMapping[] = {
-    #define X(Pin, Port, Num) \
-    { \
+#define X(Pin, Port, Num) \
+    const GpioEntry Pin = { \
         &DDR##Port, \
         &PORT##Port, \
         &PIN##Port, \
         bits::Bit##Num \
-    },
-    GPIO_DEFINES
-    #undef X
-};
-
-/**
- * Set the given Gpio Pin to given mode
- */
-inline void mode(GpioPin pin, GpioMode mode)
-{
-    if (mode == Input) {
-        bits::clear(
-            *(GpioMapping[pin].dirReg),
-            GpioMapping[pin].num);
-    } else if (mode == Output) {
-        bits::add(
-            *(GpioMapping[pin].dirReg),
-            GpioMapping[pin].num);
-    } else if (mode == InputPullUp) {
-        bits::clear(
-            *(GpioMapping[pin].dirReg),
-            GpioMapping[pin].num);
-        bits::add(
-            *(GpioMapping[pin].outReg),
-            GpioMapping[pin].num);
-    }
-}
-
-/**
- * Write given logical value to given
- * Gpio Pin (should be Output mode)
- */
-inline void write(GpioPin pin, logic value)
-{
-    bits::set(
-        *(GpioMapping[pin].outReg),
-        GpioMapping[pin].num, 
-        value);
-}
-
-/**
- * Read and return the logical value
- * of given Gpio Pin (should be in Input mode)
- */
-inline logic read(GpioPin pin)
-{
-    return bits::get(
-        *(GpioMapping[pin].inReg),
-        GpioMapping[pin].num);
-}
+    };
+GPIO_DEFINES
+#undef X
 
 }
 
